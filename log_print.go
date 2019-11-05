6bed66afe5a08a8d92cb97b591fa43a094b31c64
visionp <visionp1986@gmail.com>
2019-11-05 17:30:40 +0200

    refactoring

--- log_print.go
+++ log_print.go
@@ -6,38 +6,39 @@ import (
 
 type LogPrint struct{}
 
-func (l LogPrint) Debug(mess string, context Context) {
-	l.log("debug", mess, context)
+func (l LogPrint) Debug(mess string, context Context) error {
+	return l.log("debug", mess, context)
 }
 
-func (l LogPrint) Info(mess string, context Context) {
-	l.log("info", mess, context)
+func (l LogPrint) Info(mess string, context Context) error {
+	return l.log("info", mess, context)
 }
 
-func (l LogPrint) Notice(mess string, context Context) {
-	l.log("notice", mess, context)
+func (l LogPrint) Notice(mess string, context Context) error {
+	return l.log("notice", mess, context)
 }
 
-func (l LogPrint) Warning(mess string, context Context) {
-	l.log("warning", mess, context)
+func (l LogPrint) Warning(mess string, context Context) error {
+	return l.log("warning", mess, context)
 }
 
-func (l LogPrint) Error(mess string, context Context) {
-	l.log("error", mess, context)
+func (l LogPrint) Error(mess string, context Context) error {
+	return l.log("error", mess, context)
 }
 
-func (l LogPrint) Critical(mess string, context Context) {
-	l.log("critical", mess, context)
+func (l LogPrint) Critical(mess string, context Context) error {
+	return l.log("critical", mess, context)
 }
 
-func (l LogPrint) Alert(mess string, context Context) {
-	l.log("alert", mess, context)
+func (l LogPrint) Alert(mess string, context Context) error {
+	return l.log("alert", mess, context)
 }
 
-func (l LogPrint) Emergency(mess string, context Context) {
-	l.log("emergency", mess, context)
+func (l LogPrint) Emergency(mess string, context Context) error {
+	return l.log("emergency", mess, context)
 }
 
-func (l LogPrint) log(level string, mess string, context Context) {
+func (l LogPrint) log(level string, mess string, context Context) error {
 	fmt.Printf("Level: %s,\n Message %s,\n Context: %v \n\n", level, mess, context)
+	return nil
 }
